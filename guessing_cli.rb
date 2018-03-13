ea66d33911c31f6bfeb2b064561516f771b2a86d
William Dale <williambdale@gmail.com>
2018-03-13 21:59:24 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,6 +10,14 @@ def run_guessing_game(limit=10)
   correct_answer = rand(limit)
   count = 0 
   while count < 3 
-    
+    puts "Pick a number:"
+    choice = gets.chomp 
+    if choice == correct_answer 
+      puts "You are correct!"
+      break 
+    else 
+      puts "Guess again"
+    end 
+    i += 1 
   end 
 end 
